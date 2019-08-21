de0e94d2be9c50dcf18fb4ef52e30d07a8cfd4c8
seojihye2 <mansesjh22@naver.com>
2019-08-21 11:37:21 +0900

    시간 setinterval

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 const icon = document.getElementById("icon");
 const temp = document.getElementById("temp");
-const place = doucment.getElementById("place");
+const place = document.getElementById("place");
 const time = document.getElementById("time");
-const name = doucment.getElementById("name");
+const name = document.getElementById("name");
 const input_name = document.getElementById("input_name");
+
+function showTime() {
+    const now = new Date();
+    time.innerText = now.toLocaleTimeString().slice(3);
+}
+
+showTime();
+setInterval(showTime,1000);
