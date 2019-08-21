8ef76eea262ea504d257cf1787f7a2fcd6ad3162
seojihye2 <mansesjh22@naver.com>
2019-08-21 11:02:00 +0900

    get id to app.js file

---
+++ app.js
@@ -0,0 +1,6 @@
+const icon = document.getElementById("icon");
+const temp = document.getElementById("temp");
+const place = doucment.getElementById("place");
+const time = document.getElementById("time");
+const name = doucment.getElementById("name");
+const input_name = document.getElementById("input_name");
